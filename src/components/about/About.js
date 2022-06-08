a486e8953dc6bd4b863d4770d49bb5de4d17a98e
SosaMati <matiasezequielsosa@gmail.com>
2022-06-08 17:51:10 -0300

    about modificado

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,28 +1,20 @@
 import './about.css';
 import { CarrouselAbout } from '../corrouselAbout/CarrouselAbout';
+import globos from '../../assets/globosicono.png';
 
 export const About = () => {
     return (
         <div className="ab">
             <div className ='c-about container'>
                 <div className= 'text-about animate__animated animate__fadeInLeftBig'>
-                    <h1>About</h1>
+                    <h1>El cotillón <img  className='globosicono' src= {globos} alt='globos' /> </h1>
                     <br/>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Mauris euismod, nunc eget euismod consectetur, nunc nisi
-                        euismod, quis euismod nunc nisi euismod. Mauris euismod,
-                        nunc eget euismod consectetur, nunc nisi euismod, quis 
-                        euismod nunc nisi euismod. Mauris euismod, nunc eget
-                        euismod consectetur, nunc nisi euismod, quis euismod. 
-                        Mauris euismod, nunc eget euismod consectetur, nunc nisi
-                        euismod, quis euismod nunc nisi euismod. Mauris euismod,
-                        nunc eget euismod consectetur, nunc nisi euismod, quis
-                        euismod nunc nisi euismod. Mauris euismod, nunc eget
-                        euismod consectetur, nunc nisi euismod, quis euismod.
-                        Mauris euismod, nunc eget euismod consectetur, nunc nisi.
-                        lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Mauris euismod, nunc eget euismod consectetur, nunc nisi
-                        euismod, quis euismod nunc nisi euismod. Mauris euismod,
+                    <p> Somos un comercio familiar fundado en el año 1973 en Belén de Escobar por la señora 
+                        Ines Mercedes Papaleo. Desde sus inicios el comercio se caracterizó por comercializar 
+                        artículos de juguetería y librería, con el transcurso del tiempo y de las variaciones económicas del
+                        país, el negocio fue modificándose de acuerdo a las circunstancias, actualmente
+                        el negocio familiar comercializa productos de cotillón, disfraces, souvenirs y repostería 
+                        atendido por su hijo.
                     </p>
 
                 </div>
@@ -32,8 +24,8 @@ export const About = () => {
             </div>
 
             <div className='c-bottom'>
-                <h1>Lorem Ipsum</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem </p>
+                <h1>Nuesto local por dentro</h1>
+                <p>Contamos con una amplia variedad de productos. </p>
 
                 <CarrouselAbout />
 
